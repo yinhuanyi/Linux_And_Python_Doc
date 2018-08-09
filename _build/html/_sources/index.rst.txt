d91592e40a9a831d8b4a0b12bd713ab20ef32c05
xiaoming <xiaoming@qq.com>
2018-08-09 11:08:27 +0800

    first

--- _build/html/_sources/index.rst.txt
+++ _build/html/_sources/index.rst.txt
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to The Road for Fullstack's documentation!
+Welcome to Python And Linux documentation!
 ==================================================
 
 .. toctree::
@@ -11,9 +11,8 @@ Welcome to The Road for Fullstack's documentation!
    :caption: Contents:
 
 
-
-Indices and tables
-==================
+Indices and tables and
+======================
 
 * :ref:`genindex`
 * :ref:`modindex`
--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'The Road for Fullstack'
+project = 'The Road for Fullstack(Linux + Python)'
 copyright = '2018, robby'
 author = 'robby'
 
@@ -58,7 +58,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'ch'
+language = 'Chinese'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
